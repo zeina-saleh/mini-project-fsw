6558201d3f435954f5e46e06fa9aa3641d46c081
zeinaCodes <eye_likemoon@hotmail.com>
2023-07-21 02:53:09 +0300

    added backend

--- frontend_project/scripts/script.js
+++ frontend_project/scripts/script.js
@@ -1,11 +1,37 @@
 const pages = {}
 
-pages.base_url = "http://localhost/full-stack-mini-project/";
-
-
 pages.page_register = () => {
-    console.log("hello world")
+
+    let register_btn = document.getElementById("reg_btn");
+    register_btn.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        let first_name = document.getElementById("first_name").value;
+        let last_name = document.getElementById("last_name").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+
+        let formdata = new FormData();
+        formdata.append("first_name", first_name);
+        formdata.append("last_name", last_name);
+        formdata.append("email", email);
+        formdata.append("password", password);
+
+        let requestOptions = {
+            method: 'POST',
+            body: formdata,
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost/mini-project/backend_project/register.php", requestOptions)
+            .then(response => console.log(response.json()))
+            .catch(error => console.log(error))
+
+    })
+
 }
+
 pages.page_index = () => {
     console.log("hello world login")
 }
